e5a56c31aa2e1cdc051978fe1153aeda8d373dce
arefkaramidev <arefkrcoding@gmail.com>
2024-09-16 01:30:51 -0700

    FuniroFurnitureSection finish

--- src/components/FuniroFurnitureSection.jsx
+++ src/components/FuniroFurnitureSection.jsx
@@ -1,15 +1,54 @@
 const FuniroFurnitureSection = () => {
   return (
     <>
-      <div className="mt-20">
-        <div>
+      <div className="mt-20 flex flex-col items-center justify-between">
+        <div className="flex flex-col items-center justify-between">
           <span className="font-medium text-gray-500">
             Share your setup with
           </span>
           <span className="font-bold text-4xl">#FuniroFurniture</span>
         </div>
 
-        
+        <div className="grid grid-cols-5 mt-10 grid-rows-2 gap-y-5 gap-x-4 place-content-stretch">
+          <img
+            src="/public/img/goldun.png"
+            alt=""
+            className="col-span-1 inline-block"
+          />
+          <img
+            src="/public/img/radio.png"
+            alt=""
+            className="col-start-2 place-self-center"
+          />
+          <img
+            src="/public/img/desk.png"
+            alt=""
+            className="row-start-1 row-end-3 col-start-3 col-end-4 place-self-center  row-span-2"
+          />
+
+          <img
+            src="/public/img/bedroom.png"
+            alt=""
+            className="col-start-4 col-end-5 place-self-end"
+          />
+          <img
+            src="/public/img/desk2.png"
+            alt=""
+            className="col-start-5 col-end-6"
+          />
+          <img src="/public/img/chair.png" alt="" className="row-start-2" />
+          <img src="/public/img/desk3.png" alt="" className="row-start-2 " />
+          <img
+            src="/public/img/tablo.png"
+            alt=""
+            className="col-start-4 col-end-5"
+          />
+          <img
+            src="/public/img/kitchen.png"
+            alt=""
+            className="col-start-5 col-end-6"
+          />
+        </div>
       </div>
     </>
   );
